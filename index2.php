ee6da02e8177134b25ea2c723ebddb82991b62b7
Facundo Herman Gomez Gottschalk <facundo1988@hotmail.com>
2013-05-07 21:57:36 -0300

    Modificando el Skin
    Cargue un skin y voy borrando las cosas que no voy a utilizar

---
+++ index2.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="estilo.css" />
+<title>Documento sin título</title>
+</head>
+
+<body>
+
+<div id="contenedor">
+<header>
+    	<a href="index.html" class="logo">
+        	<span class="logo_ico"></span>
+            In<span>muebles</span>
+            <span class="slogan">AR<span>gentina</span></span>        </a>
+    	<div class="head_contact">
+          <ul>
+            <li class="top_phone">Iniciar Sesion  /  <span>Registrarse</span></li>
+            <li><a href="#" class="social_facebook"></a></li>
+            <li><a href="#" class="social_twitter"></a></li>
+            <li><a href="#" class="social_flickr"></a></li>
+            <li><a href="#" class="social_vimeo"></a></li>
+          </ul>
+  	  	</div>
+    	<div class="head_menu">
+        	<ul class="menu">
+            	<li><a href="index.html">INICIO</a></li>
+				<li><a href="index.html">Cargar Anuncio</a></li>
+				
+              
+            </ul>
+          <div class="head_search">
+            <form name="head_search" method="post" action="" />
+              <input type="submit" name="submit" id="submit" value="l" />
+              <input type="text" name="search_field" id="search_field" title="Buscar.." value="Buscar..." />              
+			</form>
+          </div>
+			<div class="responsive_menu">
+                menu <span class="pm">+</span>
+			</div>
+        </div>
+    </header>
+
+
+
+</div>
+
+
+
+
+
+
+</body>
+</html>
